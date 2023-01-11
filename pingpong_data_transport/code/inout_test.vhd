6bfe9d44d12a5493144d6ac760e9a2b23decd022
mike1417 <c108112168@nkust.edu.tw>
2023-01-11 11:53:06 +0800

    新增初版將8bit資料通過單一inout_pin腳正確傳輸至其他板子,目前資料尚有錯誤

--- pingpong_data_transport/code/inout_test.vhd
+++ pingpong_data_transport/code/inout_test.vhd
@@ -10,12 +10,13 @@ port(
 	rst : in STD_LOGIC;
 	LED			:out std_logic_vector(7 downto 0);
 	Button, start_btn 		:in  std_logic;
+	SW	: in std_logic_vector(7 downto 0);
 	data : inout STD_LOGIC
 );
 end inout_test;
 
 architecture arc_dir of inout_test is
-type pingpong_s is (pp_start, pp_start_2, pp_wait, pp_in, pp_out);
+type pingpong_s is (pp_start, pp_wait, pp_in, pp_out);
 signal pingpong_FSM :pingpong_s;
 
 
@@ -33,15 +34,23 @@ signal random                                           :integer range 0 to 25;
 
 
 signal data_in : STD_LOGIC;
-signal data_out, SW: STD_LOGIC;
+signal data_out, data_out2, SW_reg, SW_reg2: STD_LOGIC;
 signal time_cnt1, time_cnt2, time_cnt3 					:std_logic_vector(2 downto 0);
-signal LED_reg1 :std_logic_vector(7 downto 0);
-signal data_in_reg : std_logic;
+signal LED_reg1, LED_reg2  :std_logic_vector(7 downto 0);
+signal data_in_reg, start_btn_reg : std_logic;
+
+
+
+type data_in_check_s is (check1, check2, check3, check4, data_0, data_1, data_2, data_3, data_4, data_5, data_6, data_7, wait_1, wait_2, wait_3, wait_4);
+signal data_in_check, data_in_check2 :data_in_check_s;
+
+signal password : std_logic_vector(3 downto 0);
+
 begin
 
+	--LED <= LED_reg1;
 	LED <= LED_reg1;
-	
-	
+	password <= "1001";
 	clk_cnt:process(clk, rst)
 	begin
 		if rst = '1' then
@@ -84,8 +93,6 @@ LED_reg1_cnt:process(new_clk_1, rst)
 	        case pingpong_FSM is
 			    when pp_start =>
 					LED_reg1	<= "00000000";	
-				when pp_start_2 =>
-					LED_reg1	<= "00000000";	
 				when pp_wait =>
 					LED_reg1	<= "00000001";	
 				when pp_in =>
@@ -110,10 +117,10 @@ LED_reg1_cnt:process(new_clk_1, rst)
 	end process;
 	
 	process(clk, rst)
-	begin 
+	begin
 	    if rst = '1' then
 			pingpong_FSM	<=pp_start;
-			SW 	<= '0';
+			SW_reg 	<= '0';
 			data_out <= '0';
 	    elsif(Rising_edge(clk))then
 	        case pingpong_FSM is
@@ -125,34 +132,24 @@ LED_reg1_cnt:process(new_clk_1, rst)
 						pingpong_FSM <= pp_in;
 						
 					end if;
-					SW 	<= '0';
+					SW_reg 	<= '0';
 					data_out <= '0';
-					
-				when pp_start_2 =>
-					if start_btn = '1'then
-						pingpong_FSM <= pp_out;
-					
-						
-					end if;
-					SW 	<= '1';
-					data_out <= '0';
-				
 				when pp_wait =>
 					if Button = '1' and LED_reg1 = "00000001"and time_cnt1<="011"then
 						pingpong_FSM <= pp_out;
 					elsif time_cnt1>"001"then
-						pingpong_FSM <= pp_start_2;
+						pingpong_FSM <= pp_start;
 					end if; 
-					SW 	<= '1';
+					SW_reg 	<= '1';
 					data_out <= '0';
 				when pp_in =>
 					if LED_reg1 = "00000001"then
 						pingpong_FSM <= pp_wait;
 					elsif Button = '1'then
-						pingpong_FSM <= pp_start_2;	
+						pingpong_FSM <= pp_start;	
 						
 					end if;
-					SW 	<= '1';
+					SW_reg 	<= '1';
 					data_out <= '0';
 				when pp_out =>
 					if LED_reg1 = "10000000"then
@@ -161,7 +158,7 @@ LED_reg1_cnt:process(new_clk_1, rst)
 					else	
 						data_out <= '0';
 					end if;
-					SW 	<= '1';
+					SW_reg 	<= '1';
 				when others =>
 					null;
 				
@@ -171,13 +168,27 @@ LED_reg1_cnt:process(new_clk_1, rst)
 	end process;
 	
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	data_in<=data;
-d1:process(clk,rst,SW)
+d1:process(clk,rst,SW_reg2)
 begin
 	if rst='1' then
 		data_in_reg<= '0';
 	elsif clk'event and clk='1' then
-		if SW='0' then
+		if SW_reg2='0' then
 			data_in_reg <= data_in;
 
 		else 
@@ -189,7 +200,7 @@ begin
 end process d1;
 
 
-data<=data_out when (SW='1') else
+data<=data_out2 when (SW_reg2='1') else
 'Z';
 
 end arc_dir;
