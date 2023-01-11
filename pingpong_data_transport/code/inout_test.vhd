01ed425dfa9400d00f09d911426d0786c385be4f
mike1417 <c108112168@nkust.edu.tw>
2023-01-11 11:58:37 +0800

    修正8bit資料傳輸錯誤的問題,新增了等待時間來確認對方有正確收到資料

--- pingpong_data_transport/code/inout_test.vhd
+++ pingpong_data_transport/code/inout_test.vhd
@@ -37,11 +37,12 @@ signal data_in : STD_LOGIC;
 signal data_out, data_out2, SW_reg, SW_reg2: STD_LOGIC;
 signal time_cnt1, time_cnt2, time_cnt3 					:std_logic_vector(2 downto 0);
 signal LED_reg1, LED_reg2  :std_logic_vector(7 downto 0);
+signal DeBug_1, DeBug_2  :std_logic_vector(3 downto 0);
 signal data_in_reg, start_btn_reg : std_logic;
+signal time_cnt4 : integer range 0 to 200;
 
 
-
-type data_in_check_s is (check1, check2, check3, check4, data_0, data_1, data_2, data_3, data_4, data_5, data_6, data_7, wait_1, wait_2, wait_3, wait_4);
+type data_in_check_s is (check0, check01, check1, check2, check3, check4, data_0, data_1, data_2, data_3, data_4, data_5, data_6, data_7, wait_1, wait_2, wait_3, wait_4);
 signal data_in_check, data_in_check2 :data_in_check_s;
 
 signal password : std_logic_vector(3 downto 0);
@@ -49,8 +50,9 @@ signal password : std_logic_vector(3 downto 0);
 begin
 
 	--LED <= LED_reg1;
-	LED <= LED_reg1;
-	password <= "1001";
+	LED <= LED_reg2;
+	--LED <= DeBug_1 & DeBug_2;
+	password <= "1101";
 	clk_cnt:process(clk, rst)
 	begin
 		if rst = '1' then
@@ -60,7 +62,7 @@ begin
 			new_clk <= new_clk +1;
 		end if;
 	end process;
-	new_clk_1<= new_clk(25);
+	new_clk_1<= new_clk(3);
 	
 	LED_time_cnt1:process(new_clk_1, rst)
 	begin
@@ -177,6 +179,286 @@ LED_reg1_cnt:process(new_clk_1, rst)
 	
 	
 	
+	
+	
+	
+	
+	
+	process(new_clk_1, rst)
+	begin
+	    if rst = '1' then
+			data_in_check	<=check0;
+			LED_reg2 <= not LED_reg2;
+			DeBug_1 <= "0000";
+	    elsif(Rising_edge(new_clk_1))then
+	        case data_in_check is
+			    when check0=>
+					if data_in_reg = '0' then
+						data_in_check <= check01;
+					
+					else
+						data_in_check <= check0;
+						
+					end if;
+					
+					DeBug_1 <= "0001";
+			    when check01=>
+					if data_in_reg = '1' then
+						data_in_check <= check1;
+					
+					else
+--						data_in_check <= check0;
+						
+					end if;
+					DeBug_1 <= "0010";
+					
+			    when check1=>
+					if data_in_reg = password(3) then
+						data_in_check <= check2;
+					
+					else
+						data_in_check <= check0;
+						
+					end if;
+					DeBug_1 <= "0011";
+			    when check2=>
+					if data_in_reg = password(2)then
+						data_in_check <= check3;
+					
+					else
+						data_in_check <= check0;
+						
+					end if;
+					DeBug_1 <= "0100";
+			    when check3=>
+					if data_in_reg = password(1)then
+						data_in_check <= check4;
+					
+					else
+						data_in_check <= check0;
+						
+					end if;
+					DeBug_1 <= "0101";
+			    when check4=>
+					if data_in_reg = password(0)then
+						data_in_check <= data_0;
+					
+					else
+						data_in_check <= check0;
+						
+					end if;
+					DeBug_1 <= "0110";
+					
+					
+					
+			    when data_0=>
+					LED_reg2(7) <= data_in_reg;
+					data_in_check <= data_1;
+					DeBug_1 <= "0111";
+			    when data_1=>
+					LED_reg2(6) <= data_in_reg;
+					data_in_check <= data_2;
+					DeBug_1 <= "1000";
+			    when data_2=>
+					LED_reg2(5) <= data_in_reg;
+					data_in_check <= data_3;
+					DeBug_1 <= "1001";
+			    when data_3=>
+					LED_reg2(4) <= data_in_reg;
+					data_in_check <= data_4;
+					DeBug_1 <= "1010";
+			    when data_4=>
+					LED_reg2(3) <= data_in_reg;
+					data_in_check <= data_5;
+					DeBug_1 <= "1011";
+			    when data_5=>
+					LED_reg2(2) <= data_in_reg;
+					data_in_check <= data_6;
+					DeBug_1 <= "1100";
+			    when data_6=>
+					LED_reg2(1) <= data_in_reg;
+					data_in_check <= data_7;
+					DeBug_1 <= "1101";
+			    when data_7=>
+					LED_reg2(0) <= data_in_reg;
+					data_in_check <= wait_1;
+					DeBug_1 <= "1110";
+						
+				when wait_1=>
+					if data_in_reg = password(3) then
+						data_in_check <= wait_2;
+					
+					else
+						data_in_check <= check0;
+						
+					end if;
+					DeBug_1 <= "1111";
+			    when wait_2=>
+					if data_in_reg = password(2)then
+						data_in_check <= wait_3;
+					
+					else
+						data_in_check <= check0;
+						
+					end if;
+			    when wait_3=>
+					if data_in_reg = password(1)then
+						data_in_check <= wait_4;
+					
+					else
+						data_in_check <= check0;
+						
+					end if;
+			    when wait_4=>
+					if data_in_reg = password(0)then
+						data_in_check <= check0;
+					
+					else
+						data_in_check <= check0;
+						
+					end if;	
+					
+				when others =>
+					null;
+				
+				end case;
+	    end if;
+	
+	end process;
+	
+	
+	
+	output:process(new_clk_1, rst)
+	begin
+	    if rst = '1' then
+			data_in_check2	<=check0;
+			SW_reg2 	<= '0';
+			data_out2   <= '0';
+			start_btn_reg <= '0';
+			time_cnt4 <= 0;
+			DeBug_2 <= "0000";
+	    elsif(Rising_edge(new_clk_1))then
+	        case data_in_check2 is
+			    when check0=>
+					if start_btn = '1'= '1' then
+						data_out2 <= '0';
+						SW_reg2 <= '1';
+						data_in_check2 <= check01;
+						
+					else
+						SW_reg2 <= '0';
+						
+					end if;
+					DeBug_2 <= "0001";
+			    when check01=>
+					if time_cnt4 <= 25 then
+						time_cnt4 <= time_cnt4 +1;
+						data_out2 <= '0';
+						SW_reg2 <= '1';
+					else
+						data_in_check2 <= check1;
+						time_cnt4 <= 0;
+						data_out2 <= '1';
+						SW_reg2 <= '1';
+						
+					end if;
+					DeBug_2 <= "0010";
+					
+			    when check1=>
+					
+					data_in_check2 <= check2;
+					data_out2 <= password(3);
+					SW_reg2 <= '1';
+					DeBug_2 <= "0011";
+					
+			    when check2=>
+					data_in_check2 <= check3;
+					data_out2 <= password(2);
+					SW_reg2 <= '1';
+					DeBug_2 <= "0100";
+			    when check3=>
+					data_in_check2 <= check4;
+					data_out2 <= password(1);
+					SW_reg2 <= '1';
+					DeBug_2 <= "0101";
+			    when check4=>
+					data_in_check2 <= data_0;
+					data_out2 <= password(0);
+					SW_reg2 <= '1';
+					DeBug_2 <= "0110";
+					
+					
+					
+			    when data_0=>
+					data_out2 <= SW(7);
+					data_in_check2 <= data_1;
+					SW_reg2 <= '1';
+					DeBug_2 <= "0111";
+			    when data_1=>
+					data_out2 <= SW(6);
+					data_in_check2 <= data_2;
+					SW_reg2 <= '1';
+					DeBug_2 <= "1000";
+			    when data_2=>
+					data_out2 <= SW(5);
+					data_in_check2 <= data_3;
+					SW_reg2 <= '1';
+					DeBug_2 <= "1001";
+			    when data_3=>
+					data_out2 <= SW(4);
+					data_in_check2 <= data_4;
+					SW_reg2 <= '1';
+					DeBug_2 <= "1010";
+			    when data_4=>
+					data_out2 <= SW(3);
+					data_in_check2 <= data_5;
+					SW_reg2 <= '1';
+					DeBug_2 <= "1011";
+			    when data_5=>
+					data_out2 <= SW(2);
+					data_in_check2 <= data_6;
+					SW_reg2 <= '1';
+					DeBug_2 <= "1100";
+			    when data_6=>
+					data_out2 <= SW(1);
+					data_in_check2 <= data_7;
+					SW_reg2 <= '1';
+					DeBug_2 <= "1101";
+			    when data_7=>
+					data_out2 <= SW(0);
+					data_in_check2 <= wait_1;
+					SW_reg2 <= '1';
+					DeBug_2 <= "1110";
+						
+				when wait_1=>
+					
+					data_in_check2 <= wait_2;
+					data_out2 <= password(3);
+					SW_reg2 <= '1';
+					DeBug_2 <= "1111";
+					
+			    when wait_2=>
+					data_in_check2 <= wait_3;
+					data_out2 <= password(2);
+					SW_reg2 <= '1';
+			    when wait_3=>
+					data_in_check2 <= wait_4;
+					data_out2 <= password(1);
+					SW_reg2 <= '1';
+			    when wait_4=>
+					data_in_check2 <= check0;
+					data_out2 <= password(0);
+					SW_reg2 <= '1';
+			        start_btn_reg <= '0';
+				
+					
+				when others =>
+					null;
+				
+				end case;
+	    end if;
+	
+	end process;
 	
 	
 	
